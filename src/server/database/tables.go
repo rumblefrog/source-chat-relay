724f139ec583163b43c1065b349d89109415a546
RumbleFrog <xxlosttechxx@gmail.com>
2018-09-29 01:12:06 -0400

    wip(server): Redundant entity struct

--- src/server/database/tables.go
+++ src/server/database/tables.go
@@ -2,19 +2,9 @@ package database
 
 import (
 	"database/sql"
-	"time"
 
 	log "github.com/sirupsen/logrus"
 )
-
-type RelayEntities struct {
-	ID              int
-	Source          string
-	Type            int
-	ReceiveChannels string
-	SendChannels    string
-	CreatedAt       time.Time
-}
 
 func CreateTables(db *sql.DB) {
 	_, err := db.Exec("CREATE TABLE IF NOT EXISTS `relay_entities` ( `id` VARCHAR(64) NOT NULL , `type` TINYINT NOT NULL DEFAULT '0' , `receive_channels` VARCHAR(32) NOT NULL DEFAULT '' , `send_channels` VARCHAR(32) NOT NULL DEFAULT '' , `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`), INDEX (`type`)) ENGINE = InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_general_ci;")
