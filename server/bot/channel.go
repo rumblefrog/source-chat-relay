f3c19d79d035368659e9a861fa2a07e590c89551
Ron Melkhior <ron3353@gmail.com>
2018-10-01 05:07:21 +0300

    bot/channel: cleanup code

--- server/bot/channel.go
+++ server/bot/channel.go
@@ -73,11 +73,7 @@ func ChannelCommand(ctx *exrouter.Context, cmdType ChannelCmdType) {
 
 		return
 	} else {
-		if cmdType == Receive {
-			err = entity.SetReceiveChannels(repoEntity.ParseChannels(channel))
-		} else if cmdType == Send {
-			err = entity.SetReceiveChannels(repoEntity.ParseChannels(channel))
-		}
+		err = entity.SetReceiveChannels(repoEntity.ParseChannels(channel))
 
 		if err != nil {
 			log.WithField("error", err).Warn("Unable to update entity")
