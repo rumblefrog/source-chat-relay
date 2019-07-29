839abf526985c4c55204395d83af46386042d5c7
rumblefrog <contact@zhenyangli.me>
2019-07-29 01:29:06 -0400

    main: as service

--- server/main.go
+++ server/main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"os"
-	"os/signal"
-	"syscall"
-
+	"github.com/kardianos/service"
 	"github.com/rumblefrog/source-chat-relay/server/relay"
 	"github.com/sirupsen/logrus"
 
@@ -15,11 +12,13 @@ import (
 	"github.com/rumblefrog/source-chat-relay/server/database"
 )
 
+type program struct{}
+
 func init() {
 	logrus.SetLevel(logrus.InfoLevel)
 }
 
-func main() {
+func (p *program) Start(s service.Service) error {
 	logrus.Infof("Server is now running on version %s. Press CTRL-C to exit.", config.SCRVER)
 
 	config.ParseConfig()
@@ -30,13 +29,37 @@ func main() {
 	relay.Instance = relay.NewRelay()
 	relay.Instance.Listen(config.Config.General.Port)
 
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
-	<-sc
+	return nil
+}
 
+func (p *program) Stop(s service.Service) error {
 	logrus.Info("Received exit signal. Terminating.")
 
 	bot.RelayBot.Close()
 	relay.Instance.Listener.Close()
 	database.Connection.Close()
+
+	return nil
+}
+
+func main() {
+	svcConfig := &service.Config{
+		Name:        "source-chat-relay",
+		DisplayName: "Source Chat Relay",
+		Description: "Service for Source Chat Relay",
+	}
+
+	prg := &program{}
+
+	s, err := service.New(prg, svcConfig)
+
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	err = s.Run()
+
+	if err != nil {
+		logrus.Fatal(err)
+	}
 }
