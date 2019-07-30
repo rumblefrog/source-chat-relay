2bd2aaea0ce6af74fc31183a102d6e75df3400a0
rumblefrog <contact@zhenyangli.me>
2019-07-30 17:02:41 -0400

    server: 0 chan check

--- server/entity/utils.go
+++ server/entity/utils.go
@@ -10,7 +10,11 @@ import (
 func (e *Entity) ReceiveIntersectsWith(chans []int) bool {
 	for _, e := range e.ReceiveChannels {
 		for _, v := range chans {
-			if e == v || (e == -1 && v != 0) || (v == -1 && e != 0) {
+			if e == 0 || v == 0 {
+				continue
+			}
+
+			if e == v || e == -1 || v == -1 {
 				return true
 			}
 		}
