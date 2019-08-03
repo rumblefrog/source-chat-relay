b037039c8a149902071ae8196586c93d1297ea2b
rumblefrog <contact@zhenyangli.me>
2019-08-03 15:02:28 -0400

    entity: patch leaks

--- server/entity/query.go
+++ server/entity/query.go
@@ -11,6 +11,8 @@ func FetchEntity(id string) (*Entity, error) {
 	// Specify the column names for backward compat with old database
 	stmt, err := database.Connection.Prepare("SELECT `id`, `display_name`, `receive_channels`, `send_channels`, `disabled_receive_types`, `disabled_send_types`, `created_at` FROM `relay_entities` WHERE `id` = ?")
 
+	defer stmt.Close()
+
 	if err != nil {
 		return nil, err
 	}
@@ -48,6 +50,8 @@ func FetchEntity(id string) (*Entity, error) {
 func FetchEntities() ([]*Entity, error) {
 	// Specify the column names for backward compat with old database
 	rows, err := database.Connection.Query("SELECT `id`, `display_name`, `receive_channels`, `send_channels`, `disabled_receive_types`, `disabled_send_types`, `created_at` FROM `relay_entities`")
+
+	defer rows.Close()
 
 	if err != nil {
 		return nil, err
