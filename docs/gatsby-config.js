1a18adb5ae7834af2eadbcb45506c64605131b03
rumblefrog <contact@zhenyangli.me>
2019-09-03 13:07:31 -0400

    docs: PWA

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -1,11 +1,23 @@
 module.exports = {
     pathPrefix: '/source-chat-relay',
     plugins: [
-        'gatsby-theme-docz',
-        `gatsby-plugin-sharp`, {
-            resolve: `gatsby-source-filesystem`,
+        {
+            resolve: 'gatsby-plugin-manifest',
             options: {
-                path: `${__dirname}/src/pages`,
+              name: 'Source Chat Relay',
+              short_name: 'SCR',
+              start_url: '/',
+              background_color: '#1d2330',
+              theme_color: '#1fb6de',
+              display: 'standalone',
+            },
+        },
+        'gatsby-plugin-offline',
+        'gatsby-theme-docz',
+        'gatsby-plugin-sharp', {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                path: '${__dirname}/src/pages',
             },
         },
     ]
