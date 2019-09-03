c2014ecc0e6648bcb3c8ccdc56c8870d03c58dab
rumblefrog <contact@zhenyangli.me>
2019-09-03 13:25:54 -0400

    docs: icon manifest

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -4,12 +4,13 @@ module.exports = {
         {
             resolve: 'gatsby-plugin-manifest',
             options: {
-              name: 'Source Chat Relay',
-              short_name: 'SCR',
-              start_url: '/',
-              background_color: '#1d2330',
-              theme_color: '#1fb6de',
-              display: 'standalone',
+                name: 'Source Chat Relay',
+                short_name: 'SCR',
+                start_url: '/',
+                background_color: '#1d2330',
+                theme_color: '#1fb6de',
+                display: 'standalone',
+                icon: 'static/logo/cloud-computing.png'
             },
         },
         'gatsby-plugin-offline',
