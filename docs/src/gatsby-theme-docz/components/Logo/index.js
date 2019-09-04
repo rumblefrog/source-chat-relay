61687e75a6807e26474d6dffacfd47c79326b45a
rumblefrog <contact@zhenyangli.me>
2019-09-04 17:08:12 -0400

    docs: use absolute url for logo

--- docs/src/gatsby-theme-docz/components/Logo/index.js
+++ docs/src/gatsby-theme-docz/components/Logo/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import { Link } from 'docz';
 
-export const Logo = () => <Link to="/"><img src={'logo/cloud-computing_256.png'} alt="Logo" width="32" height="32" /></Link>
+export const Logo = () => <Link to="/"><img src='https://rumblefrog.me/source-chat-relay/logo/cloud-computing_256.png' alt="Logo" width="32" height="32" /></Link>
