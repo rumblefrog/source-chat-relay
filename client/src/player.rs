4fc237455c53df35c015302288b8ead2a60d8371
rumblefrog <contact@zhenyangli.me>
2021-03-19 19:13:54 -0400

    wip(client) - upsample and write pcm

--- client/src/player.rs
+++ client/src/player.rs
@@ -80,19 +80,38 @@ impl Player {
 
                 for decoded_chunk in decoded.chunks(FRAME_SIZE) {
                     let resampled = self.resample(decoded_chunk)?;
-                    let mut chunk = self.encode_chunk(&resampled)?;
 
-                    // let mut rem = vec![0u8; FRAME_SIZE * 2];
+                    println!("resampled len {}", resampled.len());
 
-                    // for (i, f) in decoded_chunk.iter().enumerate() {
-                    //     rem[2 * i] = *f as u8;
-                    //     rem[2 * i + 1] = (*f >> 8) as u8;
-                    // }
+                    let mut pcm = Vec::with_capacity(FRAME_SIZE * 2);
 
-                    // let resampled = self.resample(&decoded_chunk)?;
+                    for s in resampled {
+                        pcm.push({
+                            let mut s: f32 = s * 32768.0;
+
+                            if s > 32767.0 {
+                                s = 32767.0
+                            } else if s < -32768.0 {
+                                s = -32768.0
+                            }
+
+                            s as i16
+                        });
+                    }
+
+                    println!("pcm len {}", pcm.len());
 
                     // let mut chunk = self.encode_chunk(&resampled)?;
-                    out.append(&mut chunk);
+
+                    // 480 * 2 * 2
+                    let mut rem = vec![0u8; 3840];
+
+                    for (i, f) in pcm.iter().enumerate() {
+                        rem[2 * i] = *f as u8;
+                        rem[2 * i + 1] = (*f >> 8) as u8;
+                    }
+
+                    out.append(&mut rem);
                 }
             }
         }
